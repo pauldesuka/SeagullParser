312ff29a871293d6965a84e5e81d68e285f5e9aa
pauldesuka <pauldesuka@gmail.com>
2014-03-15 20:58:53 +0900

    Change project name

--- SeagullTextPurser/Program.cs
+++ SeagullTextPurser/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SeagullTextPurser
+namespace SeagullHTMLParser
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
